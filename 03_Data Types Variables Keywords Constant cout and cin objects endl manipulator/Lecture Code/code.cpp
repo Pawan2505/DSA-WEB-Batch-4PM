1c5a3d5934437f86ba04fe4e679796060e501172
pawan2505 <pawanaktu@gmail.com>
2025-03-06 16:53:56 +0530

    syntax

---
+++ 03_Data Types Variables Keywords Constant cout and cin objects endl manipulator/Lecture Code/code.cpp
@@ -0,0 +1,44 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+
+//     // cout << "Enter Number : ";
+
+//     // cout << "Pawan ";
+//     // cout << "Maurya";
+
+//     cout << "Pawan"<<endl;
+//     cout << "Maurya"<<endl;
+//     cout << "Hey "<<endl;
+// }
+
+// #include <iostream>
+// // using namespace std;
+// int main()
+// {
+
+//     // cin,cout,endl -> std
+//     // cout << "Enter Number : ";
+
+//     // cout << "Pawan ";
+//     // cout << "Maurya";
+
+//     std::cout << "Pawan" << std::endl;
+//     std::cout << "Maurya" << std::endl;
+//     std::cout << "Hey " << std::endl;
+// }
+
+// #include<iostream>
+
+// int main(){
+//     using namespace std;
+//     // cout << "Enter Number : ";
+
+//     // cout << "Pawan ";
+//     // cout << "Maurya";
+
+//     cout << "Pawan"<<endl;
+//     cout << "Maurya"<<endl;
+//     cout << "Hey "<<endl;
+// }
+
