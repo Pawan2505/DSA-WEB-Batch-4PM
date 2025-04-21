d6662f6c90393a5171f2cfdfda4b4372a6082329
pawan2505 <pawanaktu@gmail.com>
2025-04-21 16:52:08 +0530

    array

--- 11_Array/code.cpp
+++ 11_Array/code.cpp
@@ -267,33 +267,165 @@
 //     }
 // }
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int main()
-{
-    int row = 3;
-    int col = 4;
+// int main()
+// {
+//     int row = 3;
+//     int col = 4;
 
-    int arr[row][col];
+//     int arr[row][col];
 
-    for (int i = 0; i < row; i++)
-    {
-        for (int j = 0; j < col; j++)
-        {
-            cout << "Enter data at " << i << " row and " << j << " col :";
-            cin >> arr[i][j];
-        }
+//     for (int i = 0; i < row; i++)
+//     {
+//         for (int j = 0; j < col; j++)
+//         {
+//             cout << "Enter data at " << i << " row and " << j << " col :";
+//             cin >> arr[i][j];
+//         }
 
-    }
+//     }
 
-    cout << "Array : "<<endl;
-    for (int i = 0; i < row; i++)
-    {
-        for (int j = 0; j < col; j++)
-        {
-            cout << arr[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
+//     cout << "Array : "<<endl;
+//     for (int i = 0; i < row; i++)
+//     {
+//         for (int j = 0; j < col; j++)
+//         {
+//             cout << arr[i][j] << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+
+//     int arr[5][5] = {
+//         {11, 22, 33, 44, 55},
+//         {21, 22, 34, 45, 78},
+//         {44, 55, 67, 89, 90},
+//         {12, 34, 56, 78, 99},
+//         {11, 23, 56, 89, 95}};
+
+//     int row = 4;
+//     int col = 0;
+
+//     for (int i = 4; i >=0; i--){
+//         cout << arr[row][col] << " ";
+//         row--;
+//         col++;
+//     }
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int arr[6] = {12, -232, 45, -67, 78, 23};
+
+//     for (int i = 0; i < 6; i++)
+//     {
+//         if (arr[i] < 0)
+//         {
+//             cout << arr[i] << " ";
+//         }
+//     }
+// }
+
+// #include <iostream>
+// #include<climits>
+// using namespace std;
+
+// int main()
+// {
+
+//     int arr[5][5] = {
+//             {11, 22, 33, 44, 55},
+//             {21, 22, 34, 45, 78},
+//             {44, 55, 67, 89, 90},
+//             {12, 34, 56, 78, 99},
+//             {11, 23, 56, 89, 95}};
+
+//     int maxElement = INT_MIN;
+
+//     for (int row = 0; row < 5; row++){
+//         for (int col = 0; col < 5; col++){
+//             if(arr[row][col] > maxElement){
+//                 maxElement = arr[row][col];
+//             }
+//         }
+//     }
+
+//     cout << "Max Element : " << maxElement << endl;
+// }
+
+// #include <iostream>
+// #include<climits>
+// using namespace std;
+
+// int main()
+// {
+//     int i = 5;
+//     int j = 5;
+//     int arr[i][j] = {
+//             {11, 22, 33, 44, 55},
+//             {21, 22, 34, 45, 78},
+//             {44, 55, 67, 89, 90},
+//             {12, 34, 56, 78, 99},
+//             {11, 23, 56, 89, 95}};
+
+//     for (int j = 0; j < 5; j++){
+//         for (int i = 0; i < 5; i++){
+//             cout << arr[i][j] << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     float matrix[2][2], inverse[2][2];
+//     float det;
+
+//     // Input 2x2 matrix
+//     cout << "Enter elements of 2x2 matrix:\n";
+//     for (int i = 0; i < 2; i++)
+//         for (int j = 0; j < 2; j++)
+//             cin >> matrix[i][j];
+
+//     // Calculate determinant
+//     det = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+
+//     if (det == 0)
+//     {
+//         cout << "Matrix is not invertible (determinant is 0).\n";
+//         return 0;
+//     }
+
+//     // Inverse using formula
+//     inverse[0][0] = matrix[1][1] / det;
+//     inverse[0][1] = -matrix[0][1] / det;
+//     inverse[1][0] = -matrix[1][0] / det;
+//     inverse[1][1] = matrix[0][0] / det;
+
+//     // Display inverse
+//     cout << "Inverse matrix is:\n";
+//     for (int i = 0; i < 2; i++)
+//     {
+//         for (int j = 0; j < 2; j++)
+//             cout << inverse[i][j] << " ";
+//         cout << endl;
+//     }
+
+//     return 0;
+// }
+
+// Removing Duplicate elements from an array
+
