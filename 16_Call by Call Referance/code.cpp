db0d646ea159867870b0901c68db509effae4163
pawan2505 <pawanaktu@gmail.com>
2025-04-25 17:29:03 +0530

    code

---
+++ 16_Call by Call Referance/code.cpp
@@ -0,0 +1,34 @@
+// Call by value
+
+// #include<iostream>
+// using namespace std;
+
+// int data(int x){
+//     cout << "x : " <<x<< endl;
+// }
+
+// int main(){
+//     int x = 10;
+
+//     data(x);
+// }
+
+// Call by reference
+
+// #include<iostream>
+// using namespace std;
+
+// int data(int &x){
+//     cout << "x : " << x << endl;  // 10
+//     x = 45;
+//     cout << "x : " <<x<< endl; //45
+
+// }
+
+// int main(){
+//     int x = 10;
+
+//     data(x);
+
+//     cout << "x : " << x << endl; //45
+// }
